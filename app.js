ba6dacb17babe299e05a6053cc0e6d9ced816242
Hanif Adams <omeizahanif@gmail.com>
2019-11-28 08:51:12 +0000

    add mongolab

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const User = require('./public/src/models/userModel');
 //mongoose.connect('mongodb://localhost/nouquick');
 (function db () {
     try {
-        return mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/nouquick');
+        return mongoose.connect(process.env.MONGOLAB_URI || process.env.MONGODB_URI);
     } catch (error) {
         console.log("Error! : ", error);
     }
