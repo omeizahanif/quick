8fdd246f3433b2936e123b477e98de3efa67e3bf
Hanif Adams <omeizahanif@gmail.com>
2020-01-12 14:31:35 +0000

    modify student result model

--- public/src/routes/studentRoutes.js
+++ public/src/routes/studentRoutes.js
@@ -14,15 +14,15 @@ studentRouter.route('/').post((req, res) => {
             let studentData = await Student.findOne({matric: req.body.matric}, (err, students) => {
                 err ? console.log(err) : students;
             });
-            console.log(studentData);
-            let resultData = await Result.find({'score.matric' : req.body.matric}, (err, results) => {
+
+            let resultData = await Result.findOne({matric : req.body.matric}, (err, results) => {
                 err ? console.log(err) : results;
             });
 
             let courseData = await Course.find({}, (err, courses) => {
                 err ? console.log(err) : courses;
             })
-            //console.log(resultData);
+            
             //res.send('it works');
             res.render('studentMain', {profile: studentData, results: resultData, courses: courseData});
         } catch(err) {
